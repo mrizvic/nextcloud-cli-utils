5a41147b823f9e39c3a7d4976dd59e80cdbaf046
Marko Rizvic <marko.rizvic@gmail.com>
2020-05-26 10:55:57 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,113 @@
+#NEXTCLOUD CLI UTILS
+
+## IDEA
+Utilities are created during administration of several nextcloud instances.
+
+## REQUIREMENTS
+mysql-client, jq, curl
+
+## INSTALLATION
+One might need to adjust paths inside scripts for example edit `showconf` and change location of `/var/www/nextcloud/config/config.php` to suit your installation.
+
+Create entry in `~/.my.cnf` file to describing connection to nextcloud mysql server:
+```
+$ cat <<EOF > ~/.my.cnf
+[mysql]
+user=ncDBuser
+password=yourNCdbPassword
+database=ncDBname
+host=localhost
+prompt="(\d)\u# "
+EOF
+```
+
+Specify nextcloud database name in `~/.nextclouddb`
+```
+$ echo "ncDBname" > ~/.nextclouddb
+```
+
+Test if connectiont to database works:
+```
+$ mysql
+Reading table information for completion of table and column names
+You can turn off this feature to get a quicker startup with -A
+
+Welcome to the MariaDB monitor.  Commands end with ; or \g.
+Your MariaDB connection id is 2254
+Server version: 10.3.17-MariaDB-log MariaDB Server
+
+Copyright (c) 2000, 2018, Oracle, MariaDB Corporation Ab and others.
+
+Type 'help;' or '\h' for help. Type '\c' to clear the current input statement.
+
+(ncDBname)root# STATUS
+--------------
+mysql  Ver 15.1 Distrib 10.3.17-MariaDB, for Linux (x86_64) using readline 5.1
+
+Connection id:          2254
+Current database:       ncDBname
+Current user:           ncDBuser@localhost
+SSL:                    Not in use
+Current pager:          stdout
+Using outfile:          ''
+Using delimiter:        ;
+Server:                 MariaDB
+Server version:         10.3.17-MariaDB-log MariaDB Server
+Protocol version:       10
+Connection:             Localhost via UNIX socket
+Server characterset:    utf8mb4
+Db     characterset:    utf8mb4
+Client characterset:    utf8mb4
+Conn.  characterset:    utf8mb4
+UNIX socket:            /var/lib/mysql/mysql.sock
+Uptime:                 2 hours 47 min 28 sec
+
+Threads: 17  Questions: 111922  Slow queries: 0  Opens: 104  Flush tables: 1  Open tables: 98  Queries per second avg: 11.138
+--------------
+
+(ncDBname)root# SHOW TABLES;
++-----------------------------+
+| Tables_in_ncDBname          |
++-----------------------------+
+| oc_accounts                 |
+| oc_activity                 |
+| oc_activity_mq              |
+| oc_addressbookchanges       |
+--snip--
+| oc_vcategory                |
+| oc_vcategory_to_object      |
+| oc_whats_new                |
++-----------------------------+
+85 rows in set (0.001 sec)
+```
+
+Copy scripts to `~/bin` or `/usr/local/bin` or similar location.
+
+## USAGE
+Start by running `nc_help` and then suit yourself :)
+
+```
+$ ./nc_help
+Nextcloud scripts located in /opt/scripts:
+------------------------------------------------------------------------------------------
+clearbruteforces         "Clear entry from oc_bruteforce_attempts table"
+cloudsend.sh             "Uses curl to send files to a shared Nextcloud/Owncloud folder"
+editconf                 "edit nextcloud config.php"
+ncc                      "nextcloud cli command (occ)"
+showaudit                "Parse and list nextcloud audit.log entries"
+showbruteforces          "Show oc_bruteforce_attempts table"
+showconf                 "Show nextcloud config.php"
+showfailedlogin          "Show failed logins from nextcloud.log"
+showlog                  "Parse and list nextcloud.log entries"
+showslowlog              "Show mysql slow.log entries"
+showtagslike             "Show tagged files"
+showusage                "Show disk usage on nextcloud/data directory"
+------------------------------------------------------------------------------------------
+```
+## Disclaimer
+Use at your own risk. I cannot be held responsible for any misinterpretation of output presented to anyone using code from this repository.
+
+## Coffee
+If you find this useful you can buy me a coffee :) https://ko-fi.com/markor
+
+
---
+++ cloudsend.sh
@@ -0,0 +1,425 @@
+#!/usr/bin/env bash
+
+############################################################
+#
+# Tavinus Cloud Sender 2
+# cloudsend.sh
+# https://github.com/tavinus/cloudsend.sh
+#
+# Uses curl to send files to a shared
+# Nextcloud/Owncloud folder
+#
+# Usage: ./cloudsend.sh <file> <folderLink>
+# Help:  ./cloudsend.sh -h
+#
+# Gustavo Arnosti Neves
+# https://github.com/tavinus
+#
+# Contributors:
+# @MG2R @gessel
+#
+# Get this script to current folder with:
+# curl -O 'https://raw.githubusercontent.com/tavinus/cloudsend.sh/master/cloudsend.sh' && chmod +x cloudsend.sh
+#
+# NOTE: Cloudsend 2 changed the way password is handled and 
+# is NOT compatible with cloudsend 1 calls. The -e parameter
+# now does what -p parameter did (environment passwords), 
+# while the -p parameter receives the password directly.
+#
+############################################################
+help_string="Uses curl to send files to a shared Nextcloud/Owncloud folder"
+
+
+
+
+CS_VERSION="2.1.7"
+
+TRUE=0
+FALSE=1
+
+CLOUDURL=""
+FOLDERTOKEN=""
+
+PUBSUFFIX="public.php/webdav"
+HEADER='X-Requested-With: XMLHttpRequest'
+
+CLOUDSEND_PARAMS=()
+INSECURE=''
+OUTFILE=''
+
+RENAMING=$FALSE
+QUIETMODE=$FALSE
+GLOBBING=$FALSE
+GLOBCMD=' -g'
+
+
+
+
+
+################################################################
+#### CURL CALL EXAMPLE
+
+# https://cloud.mydomain.net/s/fLDzToZF4MLvG28
+# curl -k -T myFile.ext -u "fLDzToZF4MLvG28:" -H 'X-Requested-With: XMLHttpRequest' https://cloud.mydomain.net/public.php/webdav/myFile.ext
+
+
+
+
+
+################################################################
+#### MESSAGES
+################################################################
+
+
+# Logs message to stdout
+log() {
+	# [ "$VERBOSE" == " -s" ] || printf "%s\n" "$1"
+	isQuietMode || printf "%s\n" "$@"
+}
+
+
+# Prints program name and version
+printVersion() {
+        printf "%s\n" "Tavinus Cloud Sender v$CS_VERSION"
+}
+
+
+# Prints error messages and exits
+initError() {
+        printVersion >&2
+        printf "%s\n" "Init Error! $1" >&2
+        printf "%s\n" "Try: $0 --help" >&2
+        exit 5
+}
+
+
+# Prints usage information (help)
+usage() {
+        printVersion
+        printf "%s" "
+Parameters:
+  -h | --help              Print this help and exits
+  -q | --quiet             Disables verbose messages
+  -V | --version           Prints version and exits
+  -r | --rename <file.xxx> Change the destination file name
+  -g | --glob              Disable input file checking to use curl globs
+  -k | --insecure          Uses curl with -k option (https insecure)
+  -p | --password <pass>   Uses <pass> as shared folder password
+  -e | --envpass           Uses env var \$CLOUDSEND_PASSWORD as share password
+                           You can 'export CLOUDSEND_PASSWORD' at your system, or set it at the call
+                           Please remeber to also call -e to use the password set
+
+Notes:
+  Cloudsend 2 changed the way password works
+  Cloudsend 0.x.x used the '-p' parameter for the Environment password (changed to -e in v2+)
+  Please use EITHER -e OR -p, but not both. The last to be called will be used
+
+    Env Pass > Set the variable CLOUDSEND_PASSWORD='MySecretPass' and use the option '-e'
+  Param Pass > Send the password as a parameter with '-p <password>'
+
+Input Globbing:
+  You can use input globbing (wildcards) by setting the -g option
+  This will ignore input file checking and pass the glob to curl to be used
+  You MUST NOT rename files when globbing, input file names will be used
+  Glob examples: '{file1.txt,file2.txt,file3.txt}'
+                 'img[1-100].png'
+
+Send from stdin (pipe):
+  You can send piped content by using - or . as the input file name (curl specs)
+  You MUST set a destination file name to use stdin as input (-r <name>)
+
+  Use the file name '-' (a single dash) to use stdin instead of a given file
+  Alternately, the file name '.' (a single period) may be specified instead of '-' to use
+  stdin in non-blocking mode to allow reading server output while stdin is being uploaded
+
+Send folder examples:
+  find ./ -maxdepth 1 -type f -exec ./cloudsend.sh {} https://cloud.mydomain.tld/s/TxWdsNX2Ln3X5kxG -p yourPassword \;
+  find /home/myname/myfolder -type f -exec ./cloudsend.sh {} https://cloud.mydomain.tld/s/TxWdsNX2Ln3X5kxG -p yourPassword \;
+
+Uses:
+  ./cloudsend.sh [options] <filepath> <folderLink>
+  CLOUDSEND_PASSWORD='MySecretPass' ./cloudsend.sh -e [options] <filepath> <folderLink>
+
+Examples:
+  CLOUDSEND_PASSWORD='MySecretPass' ./cloudsend.sh -e './myfile.txt' 'https://cloud.mydomain.net/s/fLDzToZF4MLvG28'
+  ./cloudsend.sh './myfile.txt' 'https://cloud.mydomain.net/s/fLDzToZF4MLvG28'
+  ./cloudsend.sh -r 'RenamedFile.txt' './myfile.txt' 'https://cloud.mydomain.net/s/fLDzToZF4MLvG28'
+  ./cloudsend.sh -p 'MySecretPass' './myfile.txt' 'https://cloud.mydomain.net/s/fLDzToZF4MLvG28'
+  ./cloudsend.sh -p 'MySecretPass' -r 'RenamedFile.txt' './myfile.txt' 'https://cloud.mydomain.net/s/fLDzToZF4MLvG28'
+  ./cloudsend.sh -g -p 'MySecretPass' '{file1,file2,file3}' 'https://cloud.mydomain.net/s/fLDzToZF4MLvG28'
+  cat file | ./cloudsend.sh - 'https://cloud.mydomain.net/s/fLDzToZF4MLvG28' -r destFileName
+
+"
+}
+
+
+
+
+
+################################################################
+#### GET OPTIONS
+################################################################
+
+
+# Checks only for quiet/verbose mode and ignores all else
+parseQuietMode(){
+        while :; do
+                case "$1" in
+                        -q|--quiet)
+                                QUIETMODE=$TRUE
+                                VERBOSE=" -s" ; break ;;
+                        *)
+                                isEmpty "$1" && break || shift ;;
+                esac
+        done
+
+}
+
+
+# Parses CLI options and parameters
+parseOptions() {
+        while :; do
+                case "$1" in
+                        -h|--help)
+                                usage ; exit 0 ;;
+                        -V|--version)
+                                printVersion ; exit 0 ;;
+                        -q|--quiet)
+                                shift ;; # already checked
+                        -k|--insecure)
+                                INSECURE=' -k'
+                                log " > Insecure mode ON"
+                                shift ;;
+                        -e|--envpass|--environment)
+                                loadPassword "${CLOUDSEND_PASSWORD}"
+                                log " > Using password from environment"
+                                shift ;;
+                        -p|--password)
+                                loadPassword "$2"
+                                log " > Using password from parameter"
+                                shift ; shift ;;
+                        -r|--rename)
+                                loadOutFile "${2}"
+                                log " > Destination file will be renamed to \"$OUTFILE\""
+                                RENAMING=$TRUE
+                                shift ; shift ;;
+                        -g|--glob)
+                                GLOBBING=$TRUE
+                                GLOBCMD=''
+                                log " > Glob mode on, input file checkings disabled"
+                                shift ;;
+                        *)
+                                if isEmpty "$1"; then
+                                        break ;
+                                else
+                                        CLOUDSEND_PARAMS=("${CLOUDSEND_PARAMS[@]}" "$1")
+                                        shift ;
+                                fi
+                                        
+                esac
+        done
+        
+        CLOUDURL=''
+        FILENAME="${CLOUDSEND_PARAMS[0]}"
+        CLOUDSHARE="${CLOUDSEND_PARAMS[1]}"
+
+        # if we have index.php in the URL, process accordingly
+        if [[ "$CLOUDSHARE" == *"index.php"* ]]; then
+                CLOUDURL="${CLOUDSHARE%/index.php/s/*}"
+        else
+                CLOUDURL="${CLOUDSHARE%/s/*}"
+        fi
+
+        FOLDERTOKEN="${CLOUDSHARE##*/s/}"
+        
+        if isGlobbing; then
+                if isRenaming; then
+                        initError $'Cannot rename output files when using globbing on input.\nAll files would get the same output name and then be overwritten.\nSend individual files if you need renaming.'
+                elif isPiped "$FILENAME"; then
+                        initError $'Cannot use globbing and send piped input at the same time.\nDo either one or the other.'
+                fi
+        else
+                if ! isFile "$FILENAME" && ! isPiped "$FILENAME"; then
+                        initError "Invalid input file: $FILENAME"
+                fi
+
+                if isPiped "$FILENAME" && ! isRenaming; then
+                        initError $'No output file name!\nYou need to set a destination name when reading from a pipe!\nPlease add -r <filename.ext> to your call.'
+                fi
+        fi
+
+        if isEmpty "$CLOUDURL"; then
+                initError "Empty URL! Nowhere to send..."
+        fi
+
+        if isEmpty "$FOLDERTOKEN"; then
+                initError "Empty Folder Token! Nowhere to send..."
+        fi
+
+}
+
+
+# Parses password to var or exits
+loadPassword() {
+        if [ -z "$@" ]; then
+                initError "Trying to set an empty password"
+        fi
+        PASSWORD="$@"
+}
+
+
+# Parses destination file name to var or exits
+loadOutFile() {
+        if [ -z "$@" ]; then
+                initError "Trying to set an empty destination file name"
+        fi
+        OUTFILE="$@"
+}
+
+
+
+
+
+################################################################
+#### VALIDATORS
+################################################################
+
+
+# Dependency check
+checkCurl() {
+        CURLBIN="$(command -v curl 2>/dev/null)"
+        isExecutable "$CURLBIN" && return $TRUE
+        CURLBIN='/usr/bin/curl'
+        isExecutable "$CURLBIN" && return $TRUE
+        CURLBIN='/usr/local/bin/curl'
+        isExecutable "$CURLBIN" && return $TRUE
+        CURLBIN='/usr/share/curl'
+        isExecutable "$CURLBIN" && return $TRUE
+        initError "No curl found on system! Please install curl and try again!"
+        exit 6
+}
+
+
+# Returns $TRUE if $1 is a file, $FALSE otherwise
+isFile() {
+        [[ -f "$1" ]] && return $TRUE
+        return $FALSE
+}
+
+
+# Returns $TRUE if $1 is executable, $FALSE otherwise
+isExecutable() {
+        [[ -x "$1" ]] && return $TRUE
+        return $FALSE
+}
+
+
+# Returns $TRUE if $1 is empty, $FALSE otherwise
+isEmpty() {
+        [[ -z "$1" ]] && return $TRUE
+        return $FALSE
+}
+
+
+# Returns $TRUE if $1 is not empty, $FALSE otherwise
+isNotEmpty() {
+        [[ -z "$1" ]] && return $FALSE
+        return $TRUE
+}
+
+
+# Checks if the input file is stdin (either - or .)
+isPiped() {
+        if [ "$1" = '-' ] || [ "$1" = '.' ] ; then
+                return $TRUE
+        fi
+        return $FALSE        
+}
+
+
+
+
+
+################################################################
+#### FLAG CHECKERS
+################################################################
+
+
+# If we are renaming the output, return $TRUE, else $FALSE
+isRenaming() {
+        return $RENAMING
+}
+
+
+# If we are running in Quiet Mode, return $TRUE, else $FALSE
+isQuietMode() {
+        return $QUIETMODE
+}
+
+
+# If we are globbing the input, return $TRUE, else $FALSE
+isGlobbing() {
+        return $GLOBBING
+}
+
+
+
+
+
+################################################################
+#### RUNNERS
+################################################################
+
+
+# Logs succes or failure from curl
+logResult() {
+        #echo "LOGRESULT: $1"
+        local fileString="$(/usr/bin/basename $FILENAME)"
+        isRenaming && fileString="$(/usr/bin/basename $FILENAME) (renamed as $OUTFILE)"
+        if [ $1 -eq 0 ]; then
+                log " > Curl exited without errors"$'\n'" > Attempt to send completed > $fileString"
+                exit 0
+        fi
+        log " > Curl error when sending file > $fileString"
+        exit $1
+}
+
+
+# Execute curl send
+sendFile() {
+        if isGlobbing; then
+                OUTFILE=''
+        elif isEmpty "$OUTFILE"; then # If we are not renaming, use the input file name
+                OUTFILE="$(/usr/bin/basename $FILENAME)"
+        fi
+        
+        
+        # Send file
+        #echo "$CURLBIN"$INSECURE$VERBOSE -T "$FILENAME" -u "$FOLDERTOKEN":"$PASSWORD" -H "$HEADER" "$CLOUDURL/$PUBSUFFIX/$OUTFILE"
+        "$CURLBIN"$INSECURE$VERBOSE$GLOBCMD -T "$FILENAME" -u "$FOLDERTOKEN":"$PASSWORD" -H "$HEADER" "$CLOUDURL/$PUBSUFFIX/$OUTFILE"
+        logResult $?
+}
+
+
+
+
+
+################################################################
+#### RUN #######################################################
+################################################################
+parseQuietMode "${@}"
+parseOptions "${@}"
+checkCurl
+sendFile
+################################################################
+#### RUN #######################################################
+################################################################
+
+
+
+
+
+
+################################################################
+exit 88 ; # should never get here
+################################################################
